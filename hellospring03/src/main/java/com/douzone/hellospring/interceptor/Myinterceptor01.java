9ec603e2c51fbc5a123fb4692261caeae05de590
stpn94 <stpn94@gmail.com>
2021-10-22 20:06:28 +0900

    intercepter

---
+++ hellospring03/src/main/java/com/douzone/hellospring/interceptor/Myinterceptor01.java
@@ -0,0 +1,43 @@
+package com.douzone.hellospring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class Myinterceptor01 implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Object handler) 
+					throws Exception {
+		System.out.println("Myinteceptor01.preHandle(...) called");
+		return true;
+	}
+
+	@Override
+	public void postHandle(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Object handler,
+			ModelAndView modelAndView) 
+					throws Exception {
+		System.out.println("Myinteceptor01.postHandle(...) called");
+
+	}
+
+	@Override
+	public void afterCompletion(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Object handler,
+			Exception ex) 
+					throws Exception {
+		System.out.println("Myinteceptor01.afterCompletion(...) called");
+
+	}
+
+}
--- hellospring03/src/main/java/com/douzone/hellospring/interceptor/Myinterceptor02.java
+++ hellospring03/src/main/java/com/douzone/hellospring/interceptor/Myinterceptor02.java
@@ -1,17 +1,18 @@
-package com.douzone.hellospring.inteceptor;
+package com.douzone.hellospring.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class Myinteceptor02 extends HandlerInterceptorAdapter{
+public class Myinterceptor02 extends HandlerInterceptorAdapter{
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {		
+		System.out.println(handler.getClass());
 		System.out.println("MyInterceptor02.preHandle(...) called");
-		return false;
+		return true;
 				 
 
 	}
